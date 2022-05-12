5b6e0dbe8ca2c5659447c770e72557cfa268eda1
mwhittet <5453237+mwhittet@users.noreply.github.com>
2022-05-13 00:23:19 +0100

    Update dependencies to the latest versions

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,16 @@
 module.exports = {
-  transform: {
-    '^.+\\.[jt]sx?$': '<rootDir>/jest-preprocess.js',
-  },
+  globals: { __PATH_PREFIX__: '' },
   moduleNameMapper: {
     '.+\\.(css|styl|less|sass|scss)$': 'identity-obj-proxy',
     '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__mocks__/file-mock.js',
   },
   modulePathIgnorePatterns: ['\\.cache'],
-  testPathIgnorePatterns: ['node_modules', '\\.cache', '<rootDir>.*/public'],
-  transformIgnorePatterns: ['node_modules/(?!(gatsby)/)'],
-  globals: {
-    __PATH_PREFIX__: '',
-  },
-  testURL: 'http://localhost',
   setupFiles: ['<rootDir>/loadershim.js'],
-  testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/setup-test-env.js'],
+  testEnvironment: 'jest-environment-jsdom-global',
+  testEnvironmentOptions: { url: 'http://localhost' },
+  testPathIgnorePatterns: ['node_modules', '\\.cache', '<rootDir>.*/public'],
+  transform: { '^.+\\.[jt]sx?$': '<rootDir>/jest-preprocess.js' },
+  transformIgnorePatterns: ['node_modules/(?!(gatsby)/)'],
 };
