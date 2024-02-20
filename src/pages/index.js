041eb7222a62c47374d63e9c850c8f62c303ff8a
Mike Whittet <5453237+mwhittet@users.noreply.github.com>
2024-02-20 00:37:33 +0000

    Improve index page and update dependencies (#312)

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,63 +1,28 @@
-import { graphql } from 'gatsby';
-import { getImage } from 'gatsby-plugin-image';
-import { motion } from 'framer-motion';
-import { HomeWrapper, Intro, StyledImg } from '../styles/shared';
+import { HomeWrapper, Intro } from '../styles/shared';
 import { Layout, Seo } from '../components';
 
-export const query = graphql`
-  {
-    file(relativePath: { eq: "me.png" }) {
-      childImageSharp {
-        gatsbyImageData(placeholder: NONE, quality: 100)
-      }
-    }
-  }
-`;
-
-const visible = { opacity: 1, y: 0, transition: { duration: 1 } };
-
-const IndexPage = ({ data }) => {
-  const image = getImage(data.file);
-
-  return (
-    <Layout className="home">
-      <HomeWrapper
-        initial="hidden"
-        animate="visible"
-        exit={{ opacity: 0, transition: { duration: 1 } }}
-        variants={{ visible: { transition: { staggerChildren: 0.3 } } }}
+const IndexPage = () => (
+  <Layout className="home">
+    <HomeWrapper
+      initial="hidden"
+      animate="visible"
+      exit={{ opacity: 0, transition: { duration: 1 } }}
+      variants={{ visible: { transition: { staggerChildren: 0.3 } } }}
+    >
+      <Intro
+        variants={{
+          hidden: { opacity: 0, y: -50 },
+          visible: { opacity: 1, y: 0, transition: { duration: 1 } },
+        }}
       >
-        <motion.span
-          style={{
-            display: 'flex',
-          }}
-          variants={{
-            hidden: { opacity: 0, y: -50 },
-            visible,
-          }}
-        >
-          <StyledImg
-            alt="Picture of Michael Whittet"
-            image={image}
-            height="200"
-            width="200"
-          />
-        </motion.span>
-        <Intro
-          variants={{
-            hidden: { opacity: 0, y: -50 },
-            visible,
-          }}
-        >
-          <span>Hello!</span> I'm <span>Michael Whittet</span> aka Mike, Bruce,
-          Buzz, Spike, Goo, Whittet (those are some of my nicknames!) &amp; I am
-          a <span>Front End Developer</span> based just outside of{' '}
-          <span>London</span> in <span>Essex</span>.
-        </Intro>
-      </HomeWrapper>
-    </Layout>
-  );
-};
+        <span>Hello!</span> I'm <span>Michael Whittet</span> aka Mike, Bruce,
+        Buzz, Spike, Goo, Whittet (those are some of my nicknames!) &amp; I am a{' '}
+        <span>Front End Developer</span> based just outside of{' '}
+        <span>London</span> in <span>Essex</span>.
+      </Intro>
+    </HomeWrapper>
+  </Layout>
+);
 
 export default IndexPage;
 
